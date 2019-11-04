decde64e5bb5e3fd0484048971ac9e2c41fec261
Anthony Villacis <villacisanthony@yahoo.com>
2019-11-04 07:19:09 -0800

    updated budget app code

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -71,22 +71,22 @@
 
 /*******Closures******/
 
-function retirement(retirementAge) {
-  var a = ' years left until retirement';
-  return function(yearOfBirth) {
-    var age = 2016 - yearOfBirth;
-    console.log((retirementAge - age) + a);
-  }
-}
-
-var retirementUS = retirement(66);
-
-retirementUS(1990);
-
-//retirement(66)(1990);
-
-var retirementGermany = retirement(65);
-retirementGermany(1993);
+// function retirement(retirementAge) {
+//   var a = ' years left until retirement';
+//   return function(yearOfBirth) {
+//     var age = 2016 - yearOfBirth;
+//     console.log((retirementAge - age) + a);
+//   }
+// }
+//
+// var retirementUS = retirement(66);
+//
+// retirementUS(1990);
+//
+// //retirement(66)(1990);
+//
+// var retirementGermany = retirement(65);
+// retirementGermany(1993);
 
 /*
 Closure summary
@@ -104,22 +104,181 @@ function as returned
 
 //interviewQuestion done but with using Closures
 
-function interviewQuestion(job) {
-  var designerString = ', can you please explain what UX design is?';
-  var teacherString = 'What subject do you teach, ';
-  var genericString = ', what do you do?';
+// function interviewQuestion(job) {
+//   var designerString = ', can you please explain what UX design is?';
+//   var teacherString = 'What subject do you teach, ';
+//   var genericString = ', what do you do?';
+//
+//   return function(name) {
+//     if (job === 'designer') {
+//       console.log(name + designerString);
+//     } else if (job === 'teacher') {
+//       console.log(teacherString + name + '?');
+//     } else {
+//       console.log('Hello' + name + genericString );
+//     }
+//   }
+// }
+//
+//
+// var teacher = interviewQuestion('teacher');
+// teacher('John');
 
-  return function(name) {
-    if (job === 'designer') {
-      console.log(name + designerString);
-    } else if (job === 'teacher') {
-      console.log(teacherString + name + '?');
-    } else {
-      console.log('Hello' + name + genericString );
-    }
-  }
+
+
+
+/*****Bind, call and apply*****/
+
+// var john = {
+//   name: 'John',
+//   age: 26,
+//   job: 'teacher',
+//   presentation: function(style, timeOfDay) {
+//     if (style === 'formal') {
+//       console.log('Good ' + timeOfDay + ', ladies and gentlemen! I\'m ' + this.name + ' and I\'m a ' + this.job + ' and I\'m ' + this.age + ' years old.');
+//     } else if (style === 'friendly') {
+//       console.log('Hey! What\'s up? I\'m ' + this.name + ' and I\'m a ' + this.job + ' and I\'m ' + this.age + ' years old. Have a nice ' + timeOfDay);
+//     }
+//   }
+// };
+//
+// var emily = {
+//   name: 'Emily',
+//   age: 35,
+//   job: 'designer'
+// };
+//
+// john.presentation('formal', 'morning');
+//
+// //this is called method borrowing. Since we are borrowing john's
+// //presentation method
+// john.presentation.call(emily, 'friendly', 'afternoon');
+
+
+//apply method is the same but accepts arguments as an array
+//won't work below because presentation doesn't expect to receive an array
+//john.presentation.apply(emily, ['friendly', 'afternoon']);
+
+
+//this bind below presets the argument for the presentation Function
+//this presets the first argument to friendly and subsequent calls we just need to pass the second argument
+//first argument in the bind is the this variable
+// var johnFriendly = john.presentation.bind(john, 'friendly');
+//
+// johnFriendly('morning');
+// johnFriendly('night');
+//
+// var emilyFormal = john.presentation.bind(emily, 'formal');
+//
+// emilyFormal('morning');
+//
+//
+// var years = [1990, 1965, 1937, 2005, 1998];
+//
+// function arrayCalc(arr, fn) {
+//   var arrRes = [];
+//   for (var i = 0; i < arr.length; i++) {
+//     arrRes.push(fn(arr[i]));
+//   }
+//   return arrRes;
+// }
+//
+// function calculateAge(el) {
+//   return 2016 - el;
+// }
+//
+// function isFullAge(limit, el) {
+//   return el >= limit;
+// }
+//
+// var ages = arrayCalc(years, calculateAge);
+//
+// var fullJapan = arrayCalc(ages, isFullAge.bind(this, 20));
+// console.log(ages);
+// console.log(fullJapan);
+
+/*****Coding Challenge 7*****/
+
+
+//function constructors start with capital letter
+// var Question = function(question, answers, correctAnswer) {
+//   this.question = question;
+//   this.answers = answers;
+//   this.correctAnswer = correctAnswer;
+// }
+//
+// var programmingQ = new Question('How is programming?', ['tedious', 'boring', 'fun'], 2);
+//
+// var teacherQ = new Question('Who is the teacher?', ['John', 'Mark', 'Jonas'], 2);
+//
+// var lameQ = new Question('Who is lame?', ['fudge', 'smudge', 'budge'], 0);
+//
+// var allQuestions = [programmingQ, teacherQ, lameQ];
+//
+// function consoleLogQuestions () {
+//   var randomIdx = Math.floor(Math.random() * 2 + 1);
+//   console.log(allQuestions[randomIdx].question);
+//   console.log('0: ' + allQuestions[randomIdx].answers[0]);
+//   console.log('1: ' + allQuestions[randomIdx].answers[1]);
+//   console.log('2: ' + allQuestions[randomIdx].answers[2]);
+//   return allQuestions[randomIdx];
+// }
+//
+// var question = consoleLogQuestions();
+// var userAnswer = prompt(question.question);
+//
+// if (userAnswer == question.correctAnswer) {
+//   console.log('That is correct, foo!');
+// } else {
+//   console.log('Try again, foo!');
+// }
+
+
+
+//expert level
+
+
+var Question = function(question, answers, correctAnswer) {
+  this.question = question;
+  this.answers = answers;
+  this.correctAnswer = correctAnswer;
+}
+
+var programmingQ = new Question('How is programming?', ['tedious', 'boring', 'fun'], 2);
+
+var teacherQ = new Question('Who is the teacher?', ['John', 'Mark', 'Jonas'], 2);
+
+var lameQ = new Question('Who is lame?', ['fudge', 'smudge', 'budge'], 0);
+
+var allQuestions = [programmingQ, teacherQ, lameQ];
+
+function consoleLogQuestions () {
+  var randomIdx = Math.floor(Math.random() * 2 + 1);
+  console.log(allQuestions[randomIdx].question);
+  console.log('0: ' + allQuestions[randomIdx].answers[0]);
+  console.log('1: ' + allQuestions[randomIdx].answers[1]);
+  console.log('2: ' + allQuestions[randomIdx].answers[2]);
+  return allQuestions[randomIdx];
 }
 
 
-var teacher = interviewQuestion('teacher');
-teacher('John');
+var userAnswer = '';
+var score = 0;
+var correct = true;
+var question = '';
+while (userAnswer != 'exit') {
+  if (correct)
+    question = consoleLogQuestions();
+  userAnswer = prompt(question.question);
+
+  if (userAnswer == question.correctAnswer) {
+    console.log('That is correct, foo!');
+    score++;
+    console.log('Current score is: ' + score);
+    correct = true;
+  } else {
+    console.log('Try again, foo!');
+    correct = false;
+  }
+
+}
---
+++ 6-budgety/starter/app.js
@@ -0,0 +1,256 @@
+//UI Module: get input values, add the new item to UI, update UI
+//Data module: Add new item to data structure, calculate budget
+//Controller module: Add event handler, this module acts as a link between UI and data module
+
+//Module pattern
+//BUDGET CONTROLLER
+var budgetController = (function() {
+  //private add method
+
+  var Expense = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  var Income = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  // var allExpenses = [];
+  // var allIncomes = [];
+  // var totalExpenses = 0;
+  // var totalIncomes = 0;
+
+  var calculateTotal = function(type) {
+    var sum = 0;
+    data.allItems[type].forEach(function(current) {
+      sum += current.value;
+    });
+
+    data.totals[type] = sum;
+
+
+  };
+
+  var data = {
+    allItems: {
+      exp: [],
+      inc: []
+    },
+    totals: {
+      exp: 0,
+      inc: 0
+    },
+    budget: 0,
+    percentage: -1
+  }
+
+  return {
+    addItem: function(type, des, val) {
+      var newItem, ID;
+      ID = 0;
+      //we want ID = last ID + 1
+
+      //Create new ID
+      if (data.allItems[type].length > 0) {
+        ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+      } else {
+        ID = 0;
+      }
+
+
+      //create new item based on 'inc' or 'exp' type
+      if (type === 'exp') {
+        newItem = new Expense(ID, des, val);
+      } else if (type === 'inc') {
+        newItem = new Income(ID, des, val);
+      }
+
+      //Push it into our data structure
+      data.allItems[type].push(newItem);
+      //return new Element
+      return newItem;
+
+    },
+    calculateBudget: function() {
+      //calculate total income and expenses
+      calculateTotal('exp');
+      calculateTotal('inc');
+
+      //calculate the budget: income - expenses
+      data.budget =  data.totals.inc - data.totals.exp;
+
+      //calculate the percentage of income that we spend
+      if (data.totals.inc > 0) {
+        data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
+      } else {
+        data.percentage = -1;
+      }
+
+
+    },
+    getBudget: function() {
+      return {
+        budget: data.budget,
+        totalInc: data.totals.inc,
+        totalExp: data.totals.exp,
+        percentage: data.percentage
+      };
+    },
+    testing: function() {
+      console.log(data);
+    }
+  };
+
+})();
+
+
+
+
+var UIController = (function() {
+  var DOMstrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputButton: '.add__btn',
+    incomeContainer: '.income__list',
+    expenseContainer: '.expenses__list',
+    budgetLabel: '.budget__value',
+    incomeLabel: '.budget__income--value',
+    expenseLabel: '.budget__expenses--value',
+    percentageLabel: '.budget__expenses--percentage'
+  };
+
+  return {
+    getinput: function() {
+      return {
+        //will be either inc or exp
+        type: document.querySelector(DOMstrings.inputType).value,
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: parseFloat(document.querySelector(DOMstrings.inputValue).value)
+      };
+
+    },
+    addListItem: function(obj, type) {
+      var html, newHTML, element;
+      // Create HTML string with placeholder text
+
+      if (type === 'inc') {
+        element = DOMstrings.incomeContainer;
+        html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+      } else if (type === 'exp') {
+        element = DOMstrings.expenseContainer;
+        html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+      }
+
+      // Replace placeholder text with some actual Data
+      newHTML = html.replace('%id%', obj.id);
+      newHTML = newHTML.replace('%description%', obj.description);
+      newHTML = newHTML.replace('%value%', obj.value);
+
+      // Insert the HTML into the DOM
+      document.querySelector(element).insertAdjacentHTML('beforeend', newHTML);
+
+    },
+    clearFields: function() {
+      var fields, fieldsArr;
+      //this querySelectorAll returns a list and not an array so we need to convert
+      fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue);
+
+      fieldsArr = Array.prototype.slice.call(fields);
+
+      //forEach has this anonymous function with current element, the index and the whole array
+      fieldsArr.forEach(function (current, index, array) {
+        current.value = "";
+      });
+
+      //sets the cursor right back to the description portion
+      fieldsArr[0].focus();
+    },
+    displayBudget: function(obj) {
+      document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+      document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+      document.querySelector(DOMstrings.expenseLabel).textContent = obj.totalExp;
+      //don't display percentage if it's not greater than 0
+      if (obj.percentage > 0) {
+        document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage;
+      } else {
+        document.querySelector(DOMstrings.percentageLabel).textContent = '---';
+      }
+    },
+    getDOMstrings: function() {
+      return DOMstrings;
+    }
+  }
+
+})();
+
+
+//GLOBAL APP CONTROLLER
+var controller = (function(budgetCtrl, UICtrl) {
+
+  var setupEventListeners = function() {
+    var DOM = UICtrl.getDOMstrings();
+
+    document.querySelector(DOM.inputButton).addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event) {
+      //event.which is for older browsers
+      if (event.keyCode === 13 || event.which === 13) {
+        ctrlAddItem();
+      }
+    });
+
+  };
+
+  var updateBudget = function() {
+    // 1. Calculate the budget
+    budgetCtrl.calculateBudget();
+
+    // 2. return the budget
+    var budget = budgetCtrl.getBudget();
+
+    // 3. Display the budget on the UI
+    UICtrl.displayBudget(budget);
+  };
+
+  var ctrlAddItem = function() {
+    var input, newItem;
+    // 1. Get the field input Data
+    input = UICtrl.getinput();
+
+    //only add items if description and value are both not empty
+    if (input.description !== "" && !isNaN(input.value) && input.value > 0) {
+      // 2. Add the item to the budget CONTROLLER
+      newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+      // 3. Add the new item to the UI
+      UICtrl.addListItem(newItem, input.type);
+      // 4. clear the fields
+      UICtrl.clearFields();
+      // 5. Calculate and update budget__title
+      updateBudget();
+    }
+
+
+  };
+
+  return {
+    init: function() {
+      console.log('Application has started');
+      UICtrl.displayBudget({
+        budget: 0,
+        totalInc: 0,
+        totalExp: 0,
+        percentage: -1
+      });
+      setupEventListeners();
+    }
+  };
+
+})(budgetController, UIController);
+
+
+controller.init();
